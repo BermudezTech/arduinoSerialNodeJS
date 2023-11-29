de03d2ca66455a7041403ba2db1f4b8f1a86b731
JBermudezG <jebermudez587@gmail.com>
2023-11-29 13:32:10 -0500

    Transportador configurado correctamente, escritura serial

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
 
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+
 let pathPort = "";
-/*SerialPort.list()
+var grado = 0;
+var interval;
+var port = "";
+
+SerialPort.list()
     .then((ports) => {
         ports.forEach(function (port) {
             pathPort = port.path;
@@ -17,16 +27,49 @@ let pathPort = "";
     })
     .then((res) => {
         console.log(res);
-        const port = new SerialPort({
-            path: pathPort,
-            baudRate: 9600,
+        port = new SerialPort({
+            path: res,
+            baudRate: 115200,
         });
-    });*/
+
+        return port;
+    })
+    .then((port) => {
+        interval = setInterval(() => {
+            port.write(`${grado}\n`),
+                (err) => {
+                    if (err) {
+                        console.log(err.message);
+                    }
+                };
+        }, 500);
+
+        const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
+        parser.on("data", function (data) {
+            console.log(data);
+        });
+    });
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/serial", function (req, res) {
+    const dato = req.body.dato;
+    grado = dato;
+    let analog = parseInt((dato * 1023) / 270);
+    clearInterval(interval);
+    interval = setInterval(() => {
+        port.write(`${analog}\n`),
+            (err) => {
+                if (err) {
+                    console.log(err.message);
+                }
+            };
+    }, 500);
+    res.sendStatus(200);
 });
 
 app.listen(3000, function () {
--- public/transportador.js
+++ public/transportador.js
@@ -7,6 +7,8 @@ var position,
     minPoints = input.attr("min"),
     prevVal = input.attr("min");
 
+var input = document.querySelector("#posicion");
+
 range.on("mousedown touchstart", rangeSliderInit);
 range.on("mousemove touchmove", rangeSliderUpdate);
 range.on("mouseup touchend", rangeSliderStop);
@@ -53,9 +55,22 @@ function rangeSliderUpdate(e) {
             x: position.x - range.offset().left,
             y: position.y - range.offset().top,
         },
-        radius = range.width() / 2,
-        atan = Math.atan2(coords.x - radius, coords.y - radius),
-        deg = Math.ceil(-atan / (Math.PI / 180) + 180);
+        radius = range.width() / 2;
+    // var atan = Math.atan2(coords.x - radius, coords.y - radius);
+    var atan = Math.atan2(coords.y - radius, coords.x - radius);
+    // var deg = Math.ceil(-atan / (Math.PI / 180) + 180);
+    var deg = prevVal;
+
+    if (atan < 0 && atan > -Math.PI / 2)
+        deg = 90 - Math.ceil((atan * 180) / Math.PI) * -1 + 90;
+
+    if (atan < -Math.PI / 2 && atan > -Math.PI)
+        deg = Math.ceil((atan * 180) / Math.PI) + 180;
+
+    if (atan > 0 && atan < Math.PI / 2)
+        deg = Math.ceil((atan * 180) / Math.PI) + 180;
+
+    // console.log(coords.x - radius, coords.y - radius, atan, deg);
 
     if (prevVal <= 1 && lastPosition - position.x >= 0) deg = 0;
     if (prevVal >= 359 && lastPosition - position.x <= 0) deg = 360;
@@ -88,6 +103,18 @@ function rangeSliderUpdate(e) {
 
     //show value
     range.find(".count").text(`${points} °`).val(points);
+    input.value = points;
+
+    let api = "http://localhost:3000/serial";
+    const response = fetch(api, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            dato: points,
+        }),
+    });
 
     prevVal = deg;
     lastPosition = position.x;
